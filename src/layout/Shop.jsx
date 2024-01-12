38ecbe44ce8e0b70b637fd793f8928439cd1ba8a
ENNA-Egor <e.yudakov@gmail.com>
2024-01-12 17:29:51 +0300

    начало загрузки

--- src/layout/Shop.jsx
+++ src/layout/Shop.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Preloader } from '../components/Preloader';
 
 function Shop(){
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     return <main className='container content'>Shop</main>;
 }
 
