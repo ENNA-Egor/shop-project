664ab0ebdac784fad8e914e28f36c58393185a80
ENNA-Egor <e.yudakov@gmail.com>
2024-01-15 09:32:58 +0300

    Собрал загрузку но  что то не работает. Надо искать.

--- src/layout/Shop.jsx
+++ src/layout/Shop.jsx
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from 'react';
+
+import {API_KEY, API_URL} from '../config'
 import { Preloader } from '../components/Preloader';
+import {GoodsList} from '../components/GoodsList'
 
 function Shop(){
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    return <main className='container content'>Shop</main>;
+    useEffect(function getGoods () {
+            fetch (API_URL, {
+                headers : {
+                    'Authorization': API_KEY,
+                }
+            }).then((response) => response.json()).then((data)=> {
+                data.featured && setGoods(data.featured);
+                setLoading(false);
+            })
+    }, []);
+
+    return <main className='container content'>
+           { loading ? < Preloader /> : <GoodsList goods = {goods}/>}
+    </main>;
 }
 
 
